ba8f4442939bc78d30fb2ea9544b113b1f553335
miraziz772 <76822028+miraziz772@users.noreply.github.com>
2021-01-07 15:36:20 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+var clock = +prompt('введите время');
+ switch (clock) {
+    case 1:
+        alert('час ночи - нужно спать')
+        break;
+        case 2:
+            alert(' два часа ночи - нужно спать')
+            break;
+            case 3:
+                alert('три часа ночи - нужно спать')
+                break;
+                case 4:
+                    alert('четыре часа ночи - нужно спать')
+                    break;
+                    case 5:
+                        alert('Солнце яркий луч, наступил рассвет')
+                        break;
+                        case 6:
+                            alert('шесть часов утра')
+                            break;
+                            case 7:
+                                alert('просыпайся ! нужно идти в школу')
+                                break;
+                                case 8:
+                                    alert('ты уже в школе')
+                                    break;
+                                    case 9:
+                                        alert('школа')
+                                        break;
+                                        case 10:
+                                            alert('школа')
+                                            break;
+                                            case 11:
+                                                alert('скоро полдень')
+                                                break;
+                                                case 12:
+                                                    alert('полдень')
+                                                    break;
+                                                    case 13:
+                                                        alert('жраааааать пора')
+                                                        break;
+                                                        case 14:
+                                                            alert('дневной сон')
+                                                            break;
+                                                            case 15:
+                                                                alert('дневной сон')
+                                                                break;
+                                                                case 16:
+                                                                    alert('дневной сон')
+                                                                    break;
+                                                                    case 17:
+                                                                        alert('иди гулять')
+                                                                        break;
+                                                                        case 18:
+                                                                            alert('гуляй')
+                                                                            break;
+                                                                            case 19:
+                                                                                alert('гуляй')
+                                                                                break;
+                                                                                case 20:
+                                                                                    alert('если есть настроение, то сделай дз')
+                                                                                    break;
+                                                                                    case 21:
+                                                                                        alert('какой фильм сегодня будем смотреть?')
+                                                                                        break;
+                                                                                        case 22:
+                                                                                            alert('лежать в инсте')
+                                                                                            break;
+                                                                                            case 23:
+                                                                                                alert('лежать в тт')
+                                                                                                break;
+                                                                                                case 24:
+                                                                                                    alert('продолжаем лежать в тт')
+                                                                                                    break;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+     default:
+         alert('иди поспи аааааааа')
+         break;
+ }
